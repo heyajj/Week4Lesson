e3fa65d61bec5349e11f19da0b3039218a22ca56
heyajj <ajanich@ajanich-macbookpro.roam.corp.google.com>
2014-07-08 10:03:34 -0700

    Initial Commit

---
+++ Week4Lesson/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Week4Lesson
+//
+//  Created by Andrew Janich on 7/8/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
